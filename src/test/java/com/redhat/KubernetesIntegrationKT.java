72cc57a364d320e80e743b17f6ddfcf1a0ebe3a2
Torben Jaeger <torben@jit-central.com>
2018-06-18 17:14:01 +0200

    archetype generated

--- src/test/java/com/redhat/KubernetesIntegrationKT.java
+++ src/test/java/com/redhat/KubernetesIntegrationKT.java
@@ -16,10 +16,9 @@
 
 package com.redhat;
 
-import io.fabric8.arquillian.kubernetes.Session;
 import io.fabric8.kubernetes.client.KubernetesClient;
-import io.fabric8.kubernetes.api.model.Pod;
-import org.assertj.core.api.Condition;
+
+import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.arquillian.test.api.ArquillianResource;
 import org.junit.Test;
@@ -28,24 +27,14 @@ import org.junit.runner.RunWith;
 import static io.fabric8.kubernetes.assertions.Assertions.assertThat;
 
 @RunWith(Arquillian.class)
+@RunAsClient
 public class KubernetesIntegrationKT {
 
     @ArquillianResource
     KubernetesClient client;
 
-    @ArquillianResource
-    Session session;
-
     @Test
     public void testAppProvisionsRunningPods() throws Exception {
-        assertThat(client).pods()
-                .runningStatus()
-                .filterNamespace(session.getNamespace())
-                .haveAtLeast(1, new Condition<Pod>() {
-                    @Override
-                    public boolean matches(Pod podSchema) {
-                        return true;
-                    }
-                });
+        assertThat(client).deployments().pods().isPodReadyForPeriod();
     }
 }
